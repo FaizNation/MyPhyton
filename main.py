869d235bace075a7c104d511eb229f81ec450bde
Faiznation <novaatalagrab@gmail.com>
2025-02-14 23:21:26 +0700

    apa aja kekkk

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ print(greeting)
 addition = 2 + 2
 result = addition -1
 print(result)   
+
+print ('=============================')
+
+name = input('What is your name? ')
+print('Hello, ' + name + '!')
+
+print ('=============================')
+
+A = input ('Masukan Angka pertama: ')
+B = input ('Masukan Angka kedua: ')
+C = str(int(A) + int(B))
+print ('Hasil dari ' + A + ' + ' + B + ' = '+ C)
+
